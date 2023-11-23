983bc59827f6d85995f87fbd929d60b1a9fdcda0
Lukas Jerusalem <lukasjerusalem@web.de>
2023-11-23 13:26:20 +0100

    fetch request and event listener

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const submitButton = document.getElementById("submitIP");
-let ipAddress = "";
+
+
+const apiKey = '';
+const url = `https://api.ipify.org/?format=json&apiKey=${apiKey}`;
+
+async function fetchIP() {
+  alert("function fetchIP was called")
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+}
+
 
 function showIP() {
-  const ip = document.getElementById("ipAddress").value;
-  if(ip !== "") {
-    ipAddress = ip;
+  const inputIP = document.getElementById("ipAddress").value;
+  if(inputIP !== "") {
+    fetchIP();
   } else {
     alert("Please enter a valid IP address")
   }
  }
-
 
 submitButton.addEventListener("click", showIP);
